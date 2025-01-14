585c6dbb7d0207d5a95221c698861d34d79eb185
Danil Tolkachev <dandur.oak@gmail.com>
2025-01-14 14:05:48 +0300

    fix power stats keys

--- ros2_jetson_stats/ros2_jetson_stats/utils.py
+++ ros2_jetson_stats/ros2_jetson_stats/utils.py
@@ -154,7 +154,7 @@ def cpu_status(hardware, name, cpu):
     ========== ================= =======================================
     online     Bool              Status core
     governor   str               Type of governor running on the core
-    freq       dict              Frequency of the core 
+    freq       dict              Frequency of the core
     info_freq  dict              Frequency of the core
     idle_state dict              All Idle state running
     user       float             User percentage utilization
@@ -364,8 +364,8 @@ def power_status(hardware, power):
     # Make voltage diagnostic status
     d_volt = DiagnosticStatus(
         name='jetson_stats power',
-        message='curr={curr}mW avg={avg}mW'.format(
-            curr=int(power['tot']['curr']), avg=int(power['tot']['avg'])),
+        message='power={power}mW avg={avg}mW'.format(
+            power=int(power['tot']['power']), avg=int(power['tot']['avg'])),
         hardware_id=hardware,
         values=values)
     return d_volt
